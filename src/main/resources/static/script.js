fc38c6cd4fe493854ddbc8b4e0c4fc14113b1c2a
lanternarasu <lanternarasu20@gmail.com>
2023-06-01 15:29:48 +0530

    frontend-spring

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,17 @@
+function startQueueVisualizer() {
+  window.open('http://localhost:1235', '_blank');
+}
+
+function startStackVisualizer() {
+  window.open('http://localhost:1234', '_blank');
+}
+
+function startSortingVisualizer() {
+  window.open('http://localhost:1236', '_blank');
+}
+function feedback(){
+	window.open('http://localhost:3000','_blank');
+} 
+function logout(){
+		window.open('http://localhost:8088','_blank');
+}
